280f21cfd5136f396d990c836f16db77ec30a41d
gabriel <negreiragabriel@gmail.com>
2025-08-14 15:55:02 +0200

    copied the `create_colors` function from `scDNA_functions_S3.RDS` so it becomes fully independent.

--- color_palettes.R
+++ color_palettes.R
@@ -32,6 +32,53 @@ heat_col <- function(breaks){
   return(colors)
 }
 
+#create colors
+#this function takes a vector as input and returns a named vector with colors as values and name as elements from the input vector
+create_colors <-function(x, 
+                         palette = c("default", "all colors", "colorblind friendly", "fancy light", 
+                                     "fancy dark", "shades", "tarnish", "pastel", "pimp", 
+                                     "intense", "fluo", "red roses", "ochre sand", "yellow lime",
+                                     "green mint", "ice cube", "blue ocean", "indigo night", "purple wine"), 
+                         seed.use = 123){
+  #get required libraries  
+  check_required_packages("hues")
+  #test inputs
+  palette <- match.arg(palette)
+  #create pallete parameters
+  palette_param <- list(default = c(hmin = 0, hmax = 360, cmin = 30, cmax = 80, lmin = 35, lmax = 80),
+                        `all colors` = c(hmin = 0, hmax = 360, cmin = 0, cmax = 100, lmin = 0, lmax = 100),
+                        `colorblind friendly` = c(hmin = 0, hmax = 360, cmin = 40, cmax = 70, lmin = 15, lmax = 85),
+                        `fancy light` = c(hmin = 0, hmax = 360, cmin = 15, cmax = 40, lmin = 70, lmax = 100),
+                        `fancy dark` = c(hmin = 0, hmax = 360, cmin = 8, cmax = 40, lmin = 7, lmax = 40),
+                        shades = c(hmin = 0, hmax = 240, cmin = 0, cmax = 15, lmin = 0, lmax = 100),
+                        tarnish = c(hmin = 0, hmax = 360, cmin = 0, cmax = 15, lmin = 30, lmax = 70),
+                        pastel = c(hmin = 0, hmax = 360, cmin = 0, cmax = 30, lmin = 70, lmax = 100),
+                        pimp = c(hmin = 0, hmax = 360, cmin = 30, cmax = 100, lmin = 25, lmax = 70),
+                        intense = c(hmin = 0, hmax = 360, cmin = 20, cmax = 100, lmin = 15, lmax = 80),
+                        fluo = c(hmin = 0, hmax = 300, cmin = 35, cmax = 100, lmin = 75, lmax = 100),
+                        `red roses` = c(hmin = 330, hmax = 20, cmin = 10, cmax = 100, lmin = 35, lmax = 100),
+                        `ochre sand` = c(hmin = 20, hmax = 60, cmin = 20, cmax = 50, lmin = 35, lmax = 100),
+                        `yellow lime` = c(hmin = 60, hmax = 90, cmin = 10, cmax = 100, lmin = 35, lmax = 100),
+                        `green mint` = c(hmin = 90, hmax = 150, cmin = 10, cmax = 100, lmin = 35, lmax = 100),
+                        `ice cube` = c(hmin = 150, hmax = 200, cmin = 0, cmax = 100, lmin = 35, lmax = 100),
+                        `blue ocean` = c(hmin = 220, hmax = 260, cmin = 8, cmax = 80, lmin = 0, lmax = 50),
+                        `indigo night` = c(hmin = 260, hmax = 290, cmin = 40, cmax = 100, lmin = 35, lmax = 100),
+                        `purple wine` = c(hmin = 290, hmax = 330, cmin = 0, cmax = 100, lmin = 0, lmax = 40))
+  
+  x <- sort(unique(x))
+  set.seed(seed.use)
+  colors <- hues::iwanthue(n = length(x), 
+                           hmin = palette_param[[palette]]["hmin"], 
+                           hmax = palette_param[[palette]]["hmax"], 
+                           lmin = palette_param[[palette]]["lmin"], 
+                           lmax = palette_param[[palette]]["lmax"], 
+                           cmin = palette_param[[palette]]["cmin"], 
+                           cmax = palette_param[[palette]]["cmax"])
+  x <- setNames(colors, x)
+  return(x)
+}
+
+
 
 #colors for strains
 strain_colors <- scico(n = 5, palette = "bam")[c(2,4)]
